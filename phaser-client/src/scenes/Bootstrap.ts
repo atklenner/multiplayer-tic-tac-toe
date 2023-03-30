061a1e0eb2c290dcae02de7317734f727058a00c
Andrew Klenner <atklenner@gmail.com>
2023-03-30 11:15:56 -0700

    create bootstrap scene

---
+++ phaser-client/src/scenes/Bootstrap.ts
@@ -0,0 +1,12 @@
+import Phaser from "phaser";
+
+export default class Bootstrap extends Phaser.Scene {
+  constructor() {
+    super("bootstrap");
+  }
+
+  create() {
+    console.log("bootstrap scene");
+    this.scene.launch("game");
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,24 @@
-/**
- * IMPORTANT: 
- * ---------
- * Do not manually edit this file if you'd like to use Colyseus Arena
- * 
- * If you're self-hosting (without Arena), you can manually instantiate a
- * Colyseus Server as documented here: 👉 https://docs.colyseus.io/server/api/#constructor-options 
- */
-import { listen } from "@colyseus/arena";
+import http from "http";
+import express from "express";
+import cors from "cors";
+import { Server } from "colyseus";
+import { monitor } from "@colyseus/monitor";
 
-// Import arena config
-import arenaConfig from "./arena.config";
+const port = Number(process.env.PORT || 2567);
+const app = express();
 
-// Create and listen on 2567 (or PORT environment variable.)
-listen(arenaConfig);
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const gameServer = new Server({
+  server,
+});
+
+// register your room handlers
+
+// register colyseus monitor AFTER registering your room handlers
+app.use("/colyseus", monitor());
+
+gameServer.listen(port);
+console.log(`Listening on ws://localhost:${port}`);
