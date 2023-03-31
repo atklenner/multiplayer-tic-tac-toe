6cfcbfa2a327e5df2a44dc10688497385bcb00c2
Andrew Klenner <atklenner@gmail.com>
2023-03-31 12:50:55 -0700

    game room executes player selection when client sends the message

--- server/src/rooms/TicTacToe.ts
+++ server/src/rooms/TicTacToe.ts
@@ -1,8 +1,20 @@
 import { Room } from "colyseus";
+import { Dispatcher } from "@colyseus/command";
 import TicTacToeState from "./schema/TicTacToeState";
+import { Message } from "../../../types/messages";
+import PlayerSelectionCommand from "../commands/PlayerSelectionCommand";
 
 export default class TicTacToe extends Room<TicTacToeState> {
+  private dispatcher = new Dispatcher(this);
+
   onCreate() {
     this.setState(new TicTacToeState());
+
+    this.onMessage(Message.PlayerSelection, (client, message) => {
+      this.dispatcher.dispatch(new PlayerSelectionCommand(), {
+        client,
+        index: message.index,
+      });
+    });
   }
 }
