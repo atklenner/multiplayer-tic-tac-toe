1bf397651aa0483db1b00bf0c048157ee438bfaf
Andrew Klenner <atklenner@gmail.com>
2023-03-31 12:53:47 -0700

    console log server state changes, doesn't work right now

--- phaser-client/src/services/Server.ts
+++ phaser-client/src/services/Server.ts
@@ -1,10 +1,14 @@
-import { Client } from "colyseus.js";
+import { Client, Room } from "colyseus.js";
 import Phaser from "phaser";
 import ITicTacToeState from "../../../types/ITicTacToeState";
+import { Schema } from "@colyseus/schema";
+import { Message } from "../../../types/messages";
 
 export default class Server {
   private client: Client;
   private events: Phaser.Events.EventEmitter;
+
+  private room?: Room<ITicTacToeState & Schema>;
 
   constructor() {
     this.client = new Client("ws://localhost:2567");
@@ -12,11 +16,29 @@ export default class Server {
   }
 
   async join() {
-    const room = await this.client.joinOrCreate<ITicTacToeState>("tic-tac-toe");
+    this.room = await this.client.joinOrCreate<ITicTacToeState & Schema>(
+      "tic-tac-toe"
+    );
 
-    room.onStateChange.once((state) => {
+    this.room.onStateChange.once((state) => {
       this.events.emit("once-state-changed", state);
     });
+
+    this.room.state.onChange = (changes) => {
+      changes.forEach((change) => {
+        console.log(change);
+      });
+    };
+  }
+
+  makeSelection(idx: number) {
+    if (!this.room) {
+      // should throw an error
+      console.log("there is no room");
+      return;
+    }
+
+    this.room.send(Message.PlayerSelection, { index: idx });
   }
 
   onceStateChanged(cb: (state: ITicTacToeState) => void, context?: any) {
