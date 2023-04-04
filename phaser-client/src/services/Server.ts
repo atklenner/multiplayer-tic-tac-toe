ab7f552481e55232a96f48505ef6582865c32b80
Andrew Klenner <atklenner@gmail.com>
2023-04-04 12:03:47 -0700

    add message from server to client for when player turn changes

--- phaser-client/src/services/Server.ts
+++ phaser-client/src/services/Server.ts
@@ -3,12 +3,18 @@ import Phaser from "phaser";
 import ITicTacToeState from "../../../types/ITicTacToeState";
 import { Schema } from "@colyseus/schema";
 import { Message } from "../../../types/messages";
+import { Events } from "../../../types/events";
 
 export default class Server {
   private client: Client;
   private events: Phaser.Events.EventEmitter;
 
   private room?: Room<ITicTacToeState & Schema>;
+  private _playerIndex = -1;
+
+  get playerIndex() {
+    return this._playerIndex;
+  }
 
   constructor() {
     this.client = new Client("ws://localhost:2567");
@@ -18,16 +24,22 @@ export default class Server {
   async join() {
     this.room = await this.client.joinOrCreate<ITicTacToeState>("tic-tac-toe");
 
-    this.room.onStateChange.once((state) => {
-      this.events.emit("once-state-changed", state);
+    this.room?.onMessage(
+      Message.PlayerIndex,
+      (message: { playerIndex: number }) =>
+        (this._playerIndex = message.playerIndex)
+    );
+
+    this.room?.onStateChange.once((state) => {
+      this.events.emit(Events.OnceStateChange, state);
     });
 
     this.room.state.onChange = (changes) => {
       changes.forEach((change) => {
         const { field, value } = change;
         switch (field) {
-          case "":
-            this.events.emit("", value);
+          case "activePlayer":
+            this.events.emit(Events.PlayerTurnChange, value);
             break;
 
           default:
