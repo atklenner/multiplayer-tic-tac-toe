f47f877089ea2fa2a5768322ee295b8e95bcc813
Andrew Klenner <atklenner@gmail.com>
2023-04-04 11:01:14 -0700

    add colyseus schema to game state type

--- types/ITicTacToeState.ts
+++ types/ITicTacToeState.ts
@@ -1,10 +1,13 @@
+import type { Schema, ArraySchema } from "@colyseus/schema";
+
 export enum Cell {
   Empty,
   X,
   O,
 }
-export interface ITicTacToeState {
-  board: number[];
+
+export interface ITicTacToeState extends Schema {
+  board: ArraySchema<Cell>;
   activePlayer: number;
 }
 
