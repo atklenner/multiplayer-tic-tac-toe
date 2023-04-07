2f558efafab48bb0f2bf249717cb851c4ea0c7c9
Andrew Klenner <atklenner@gmail.com>
2023-04-07 10:43:17 -0700

    get an ugly modal to show up

--- react-phaser/src/App.tsx
+++ react-phaser/src/App.tsx
@@ -1,7 +1,17 @@
+import "./App.css";
 import "./PhaserGame";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="modal">
+        <h1>Let's Play a Game!</h1>
+        <button>Tic-Tac-Toe</button>
+        <button>Uno</button>
+        <button>Create a Room</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
