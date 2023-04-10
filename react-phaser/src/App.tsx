2f35755e27a98d27167bd13e8a12ed64a4618eb6
Andrew Klenner <atklenner@gmail.com>
2023-04-10 11:27:58 -0700

    add all modal types

--- react-phaser/src/App.tsx
+++ react-phaser/src/App.tsx
@@ -1,19 +1,31 @@
-import { useState } from "react";
 import "./App.css";
+import GameOver from "./components/GameOver";
+import Login from "./components/Login";
 import Modal from "./components/Modal";
+import PickAGame from "./components/PickAGame";
 import phaserGame from "./PhaserGame";
 import Bootstrap from "./scenes/Bootstrap";
+import reactStore from "./services/zustand";
 
 function App() {
-  const [viewModal, setViewModal] = useState(true);
-  const handleClick = () => {
-    const scene = phaserGame.scene.keys.bootstrap as Bootstrap;
-    setViewModal(false);
-    scene.createNewGame();
-  };
+  const { login, pickGame, gameOver } = reactStore();
   return (
     <div className="App">
-      {viewModal && <Modal handleClick={handleClick} />}
+      {login && (
+        <Modal>
+          <Login />
+        </Modal>
+      )}
+      {pickGame && (
+        <Modal>
+          <PickAGame />
+        </Modal>
+      )}
+      {gameOver && (
+        <Modal>
+          <GameOver />
+        </Modal>
+      )}
     </div>
   );
 }
